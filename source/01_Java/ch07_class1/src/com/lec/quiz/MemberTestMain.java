a5d7a0ce3c7b657de4b1d67f7701350d0ad37810
Joyfuler <jpju99@gmail.com>
2023-07-03 17:53:02 +0900

    sixth

--- source/01_Java/ch07_class1/src/com/lec/quiz/MemberTestMain.java
+++ source/01_Java/ch07_class1/src/com/lec/quiz/MemberTestMain.java
@@ -8,7 +8,7 @@ public class MemberTestMain {
 		m1.infoString();
 		System.out.println("--------이하 배열---------");
 
-		Member[] m2 = { new Member("aaa", "홍길동", "hong@company.com", "서울 강남구", "2000-01-01", '남'),
+		Member[] m2 = { m1,
 				new Member("bbb", "신길동", "shin@company.com", "서울 강남구", "2000-11-01", '남') };
 
 		// for (int idx = 0; idx < m2.length; idx++) {
