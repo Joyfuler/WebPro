1f7534f6418bf340736cc616c2d1d13c6b4baa84
Joyfuler <jpju99@gmail.com>
2023-08-24 18:13:29 +0900

    41th

--- source/04_JavaScript/ch06_객체/1_객체와배열.js
+++ source/04_JavaScript/ch06_객체/1_객체와배열.js
@@ -15,13 +15,13 @@ class Person {
     자바의 경우 person.name = '김길동'; 식으로 바꿀 수가 없으므로 (private)
     person.setName("김길동"); 과 같은 식으로 사용하였음.
 */
-const person = {'name':'홍길동','age':20}; // 자바스크립트의 경우는 다음과 같이 생성함.
+const person = { name: "홍길동", age: 20 }; // 자바스크립트의 경우는 다음과 같이 생성함.
 // name과 age는 속성변수이자 key값. (key값으로 해당 값을 가져올 수 있음.)
 // 혹은 배열로 만들어도 같은 결과가 나옴.
-personArr = ['홍길동',20];
+personArr = ["홍길동", 20];
 // 배열의 경우에는 key값은 배열의 index. personArr[0] 등으로 접근가능.
 
 person.name = "김길동"; // const 변수임에도 안의 name을 변경할 수 있음!
 // 안에 있는 객체의 값이 아니라, 참조하고 있는 주소의 값만 바꾸지 못하는 것이기 때문.
-console.log(person.name , '/' , person.age);
-console.log(person['name'], '/' , person['age']);
+console.log(person.name, "/", person.age);
+console.log(person["name"], "/", person["age"]);
--- source/04_JavaScript/ch06_객체/4_replaceAll.js
+++ source/04_JavaScript/ch06_객체/4_replaceAll.js
@@ -1,4 +1,3 @@
-
 // 위와 같은 str 변수의 0을 x로 바꾸고 싶다면?
 // var str = '안녕00방가00좋은 수요일00내일은 목요일'
 // console.log ('원본 : ' + str);
@@ -8,18 +7,15 @@
 // var joinStr = splitStr.join('x'); // join은 각 배열들을 x라는 단어로 묶는 함수.
 // console.log('Join된 Str : ' + joinStr);
 
-
-function replaceAll(txt,a,b){ // 모든 텍스트와 기존 텍스트, 바꿀 텍스트 총 3개의 매개변수를 준비.
-    var splitStr = txt.split(a); // a라는 단어를 기준으로 배열로 쪼갬.
-    var joinStr = splitStr.join(b); // 쪼개진 배열에 b를 다시 넣어 결과적으로 a가 b로 변경됨.
-    return txt.split(a).join(b);
+function replaceAll(txt, a, b) {
+  // 모든 텍스트와 기존 텍스트, 바꿀 텍스트 총 3개의 매개변수를 준비.
+  var splitStr = txt.split(a); // a라는 단어를 기준으로 배열로 쪼갬.
+  var joinStr = splitStr.join(b); // 쪼개진 배열에 b를 다시 넣어 결과적으로 a가 b로 변경됨.
+  return txt.split(a).join(b);
 }
 // 위 3줄짜리를 1줄로 바꾸는 방법
 
-
-
-
-    // var replaceStr= replaceAll(str,'0','!');
-    // console.log(replaceStr);
-    // var replaceStr2 = replaceAll(str,'0',''); //0을 아예 빼버리는 과정.
-    // console.log(replaceStr2);
+// var replaceStr= replaceAll(str,'0','!');
+// console.log(replaceStr);
+// var replaceStr2 = replaceAll(str,'0',''); //0을 아예 빼버리는 과정.
+// console.log(replaceStr2);
